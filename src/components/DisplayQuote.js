f0670473413a2bb8f273a4602f50e2e62af71eb1
John Murianki <mcjthiongo@gmail.com>
2023-06-03 20:49:46 +0300

    Add Quote

---
+++ src/components/DisplayQuote.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const Quote = () => {
+  const [quoteData, setQuoteData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const apiKey = 'Z/jC41Y2tB1gmkr21J31oQ==Zp7GxADGcmY7LfUe';
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/quotes?category=government', {
+          headers: { 'X-Api-Key': apiKey },
+        });
+        setQuoteData(response.data[0] || {});
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  let quoteContent;
+  if (isLoading) {
+    quoteContent = <p>Fetching Quote......</p>;
+  } else if (error) {
+    quoteContent = (
+      <p>
+        Error:
+        {error}
+      </p>
+    );
+  } else if (quoteData.quote && quoteData.author) {
+    quoteContent = (
+      <div>
+        <p>{quoteData.quote}</p>
+        <p>
+          <span>Author: </span>
+          {quoteData.author}
+        </p>
+      </div>
+    );
+  } else {
+    quoteContent = <p>No quote available</p>;
+  }
+
+  return <div>{quoteContent}</div>;
+};
+
+export default Quote;
