70b99c7aaec130252f34a7c6be7757d8bb75ebd0
John Murianki <mcjthiongo@gmail.com>
2023-06-14 18:50:56 +0300

    Create full Website

---
+++ src/components/QuotePage.js
@@ -0,0 +1,9 @@
+import Quote from './DisplayQuote';
+import styles from '../styles/Quotepage.module.css';
+
+const QuotePage = () => (
+  <div className={styles.qoutecontainer}>
+    <Quote />
+  </div>
+);
+export default QuotePage;
